8b37d72be00ee0303b05fad23b665e2a369d854e
jk-gan <junkai@hey.com>
2022-05-25 12:43:30 +0800

    feat: pipeline command interface

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use clap::Parser;
 use command_group::{pipeline::PipelineSubcommand, CommandGroup};
 use indicatif::{HumanDuration, MultiProgress, ProgressBar, ProgressStyle};
 use std::fmt::Display;
+use std::process::Command;
+use std::str;
 use std::thread;
 use std::time::{Duration, Instant};
 use tabled::{Table, Tabled};
@@ -27,6 +29,28 @@ fn fmt_option<T: Display>(o: &Option<T>) -> String {
 
 #[derive(Tabled)]
 struct PipelineData {
+    name: &'static str,
+    #[tabled(display_with = "fmt_option")]
+    last_succeed_at: Option<&'static str>,
+    #[tabled(display_with = "fmt_option")]
+    last_failed_at: Option<&'static str>,
+    #[tabled(display_with = "fmt_option")]
+    last_duration: Option<&'static str>,
+}
+
+#[derive(Tabled)]
+struct PipelineBranch {
+    name: &'static str,
+    #[tabled(display_with = "fmt_option")]
+    last_succeed_at: Option<&'static str>,
+    #[tabled(display_with = "fmt_option")]
+    last_failed_at: Option<&'static str>,
+    #[tabled(display_with = "fmt_option")]
+    last_duration: Option<&'static str>,
+}
+
+#[derive(Tabled)]
+struct PipelinePullRequest {
     name: &'static str,
     #[tabled(display_with = "fmt_option")]
     last_succeed_at: Option<&'static str>,
@@ -81,20 +105,86 @@ fn main() {
                 ];
 
                 let table = Table::new(pipelines).to_string();
-                print!("{table}");
+                println!("{table}");
 
                 println!("Fetch in {}.", HumanDuration(started.elapsed()));
 
                 todo!()
             }
             PipelineSubcommand::Describe { name } => {
-                println!("Describe pipeline");
-                println!("{}", name);
+                let deps = 1234;
+                let progress_bar = ProgressBar::new(deps);
+                progress_bar.set_style(ProgressStyle::default_spinner());
+                println!("Fetching pipeline data ...");
+                for _ in 0..deps {
+                    progress_bar.inc(1);
+                    thread::sleep(Duration::from_millis(3));
+                }
+                progress_bar.finish_and_clear();
+
+                println!("Pipeline \"{}\":\n", name);
+
+                let branches = vec![PipelineBranch {
+                    name: "master",
+                    last_succeed_at: Some("1 hr 20 min"),
+                    last_failed_at: Some("6 days 19 hr"),
+                    last_duration: Some("14 min"),
+                }];
+
+                let table = Table::new(branches).to_string();
+
+                println!("Branches");
+                println!("{table}");
+
+                let pull_requests = vec![
+                    PipelinePullRequest {
+                        name: "PR-3985",
+                        last_succeed_at: None,
+                        last_failed_at: None,
+                        last_duration: None,
+                    },
+                    PipelinePullRequest {
+                        name: "PR-4037",
+                        last_succeed_at: Some("1 hr 20 min"),
+                        last_failed_at: Some("6 days 19 hr"),
+                        last_duration: Some("14 min"),
+                    },
+                    PipelinePullRequest {
+                        name: "PR-4086",
+                        last_succeed_at: Some("1 min 2 sec"),
+                        last_failed_at: None,
+                        last_duration: Some("2.3 sec"),
+                    },
+                    PipelinePullRequest {
+                        name: "PR-4096",
+                        last_succeed_at: Some("1 min 2 sec"),
+                        last_failed_at: None,
+                        last_duration: Some("4.3 sec"),
+                    },
+                    PipelinePullRequest {
+                        name: "PR-4113",
+                        last_succeed_at: Some("18 hr"),
+                        last_failed_at: None,
+                        last_duration: Some("34 sec"),
+                    },
+                ];
+
+                let table = Table::new(pull_requests).to_string();
+
+                println!("Pull Requests");
+                println!("{table}");
 
                 todo!()
             }
             PipelineSubcommand::CiStatus => {
                 println!("CI Status");
+
+                let output = Command::new("git")
+                    .args(["branch", "--show-current"])
+                    .output()
+                    .expect("failed to execute process");
+
+                println!("{:?}", str::from_utf8(&output.stdout));
 
                 todo!()
             }
