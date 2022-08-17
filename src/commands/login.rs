148bbd3153d4e28af3ebb5027ff09e0f86b5bfed
jk-gan <junkai@hey.com>
2022-08-17 15:58:03 +0800

    fix: fix the login command do nothing when the auth config is empty

--- src/commands/login.rs
+++ src/commands/login.rs
@@ -23,28 +23,34 @@ pub async fn handle_login<'a>(context: GlobalContext) -> Result<bool, CliError<'
             .interact()
             .unwrap();
 
+        // selecting "Log in with a new account"
         if selection == 1 {
-            let auth_info = auth::login().await?;
-
-            let config_file = CONFIG_FILE
-                .as_ref()
-                .expect("Unable to identify user's home directory");
-
-            match CLIConfig::load(&config_file) {
-                Ok(mut config) => {
-                    config.auth = Some(AuthConfig {
-                        account: auth_info.account.clone(),
-                        access_token: auth_info.access_token,
-                        expiry_time: auth_info.expiry_time,
-                        refresh_token: auth_info.refresh_token,
-                    });
-                    config.save(&config_file).unwrap();
-                    println!("You are now logged in as [{}].", auth_info.account);
-                }
-                Err(_err) => todo!(),
-            };
+            login_and_update_config().await?;
         }
+    } else {
+        login_and_update_config().await?;
     }
+
+    Ok(true)
+}
+
+async fn login_and_update_config<'a>() -> Result<bool, CliError<'a>> {
+    let auth_info = auth::login().await?;
+
+    let config_file = CONFIG_FILE
+        .as_ref()
+        .expect("Unable to identify user's home directory");
+
+    CLIConfig::load(&config_file).map(|mut config| {
+        config.auth = Some(AuthConfig {
+            account: auth_info.account.clone(),
+            access_token: auth_info.access_token,
+            expiry_time: auth_info.expiry_time,
+            refresh_token: auth_info.refresh_token,
+        });
+        config.save(&config_file).unwrap();
+        println!("You are now logged in as [{}].", auth_info.account);
+    })?;
 
     Ok(true)
 }
