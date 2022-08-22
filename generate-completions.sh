f59b94401bb1d142b3eb37c4ca64d350171e4914
jk-gan <junkai@hey.com>
2022-08-22 13:04:36 +0800

    fix: fix the zsh completion

--- generate-completions.sh
+++ generate-completions.sh
@@ -17,7 +17,13 @@ mkdir -p completions
 gen() {
   outdir="completions/$1"
   mkdir -p ./completions/$1
-  $wukong_command completions $1 > ./completions/$1/$2
+  if [ $1 = 'zsh' ]; then
+    # replace the last line so the zsh autocompletion is source-able
+    # https://github.com/starship/starship/issues/2806
+    $wukong_command completions $1 | sed '$s/_wukong "$@"/compdef _wukong wukong/' > ./completions/$1/$2
+  else
+    $wukong_command completions $1 > ./completions/$1/$2
+  fi
   echo "$1 completions generated successfully at $( cd "$(dirname "$0")" ; pwd -P )/completions/$1/$2."
 }
 
