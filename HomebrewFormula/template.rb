19a8b06a3cabd9537656e5ac864f77de43e15ed3
jk-gan <junkai@hey.com>
2022-09-07 15:45:50 +0800

    cd: update homebrew formula after published a new release

---
+++ HomebrewFormula/template.rb
@@ -0,0 +1,40 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+
+require_relative "lib/github_private_repo_download_strategy"
+
+class Wukong < Formula
+  desc "A Swiss-army Knife CLI For Mindvalley Developers"
+  homepage "https://github.com/mindvalley/wukong-cli"
+  version __VERSION__
+
+  on_macos do
+    on_arm do
+      url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-macOS-arm.tar.gz", 
+        :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 __SHA256_MAC_ARM__
+    end
+
+    on_intel do
+      url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-macOS-x86.tar.gz",
+        :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 __SHA256_MAC_X86__
+    end
+  end
+
+  on_linux do
+    url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-linux-x86.tar.gz",
+        :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 __SHA256_LINUX_X86__
+  end
+
+
+  def install
+    bin.install "wukong"
+
+    bash_completion.install "completions/bash/wukong.bash"
+    zsh_completion.install "completions/zsh/_wukong"
+    fish_completion.install "completions/fish/wukong.fish"
+  end
+end
+
