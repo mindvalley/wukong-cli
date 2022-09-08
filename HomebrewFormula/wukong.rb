4478873429b099121986bd378eaa16ac287ebf9c
jk-gan <junkai@hey.com>
2022-09-08 15:17:55 +0800

    cd: strip the binary before creating tarball

--- HomebrewFormula/wukong.rb
+++ HomebrewFormula/wukong.rb
@@ -1,8 +1,6 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 
-require_relative "lib/github_private_repo_download_strategy"
-
 class Wukong < Formula
   desc "A Swiss-army Knife CLI For Mindvalley Developers"
   homepage "https://github.com/mindvalley/wukong-cli"
@@ -10,21 +8,18 @@ class Wukong < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-macOS-arm.tar.gz", 
-        :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-macOS-arm.tar.gz"
       sha256 "76c2c8697361aac1811545ac71d7055895cdc611eecef98202ed0a2d14878ef7"
     end
 
     on_intel do
-      url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-macOS-x86.tar.gz",
-        :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-macOS-x86.tar.gz"
       sha256 "eb4879a1f614b5cea6e7c1cbf6eaeff16a6d7603e0500038ab2d7f47bddda9ab"
     end
   end
 
   on_linux do
-    url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-linux-x86.tar.gz",
-        :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/mindvalley/wukong-cli/releases/download/#{version}/wukong-v#{version}-linux-x86.tar.gz"
     sha256 "3a79fb67d26104c817a1f8fb17a30ad37fa5d98d448e1ebf1ee8afa76b0ae067"
   end
 
