3d0606dc5d79bf12e1b3f9aede18a75457c5297f
jk-gan <junkai@hey.com>
2022-10-05 16:15:21 +0800

    test: add test case for ErrorOutput

--- src/output/error.rs
+++ src/output/error.rs
@@ -30,3 +30,78 @@ impl<'a> Display for ErrorOutput<'a> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::error::{APIError, ConfigError};
+
+    use super::*;
+
+    fn error_title_in_red() -> &'static str {
+        "\u{1b}[31mError\u{1b}[39m:"
+    }
+
+    fn caused_by_title_in_rgb_245_245_245() -> &'static str {
+        "\u{1b}[38;2;245;245;245mCaused by\u{1b}[39m:"
+    }
+
+    fn suggestion_title_in_cyan() -> &'static str {
+        "\u{1b}[36mSuggestion\u{1b}[39m:"
+    }
+
+    fn error_output_with_caused_by_and_suggestion(error: &str, caused_by: &str, suggestion: &str) -> String {
+        format!(
+            "{}\n\t{}\n\n{}\n\t{}\n\n{}\n\t{}\n",
+            error_title_in_red(),
+            error,
+            caused_by_title_in_rgb_245_245_245(),
+            caused_by,
+            suggestion_title_in_cyan(),
+            suggestion
+        )
+    }
+
+    fn error_output_with_suggestion(error: &str, suggestion: &str) -> String {
+        format!(
+            "{}\n\t{}\n\n{}\n\t{}\n",
+            error_title_in_red(),
+            error,
+            suggestion_title_in_cyan(),
+            suggestion
+        )
+    }
+
+    #[test]
+    fn test_cli_error_output_format_with_suggestion() {
+        let error = CliError::UnInitialised;
+        let error_output = ErrorOutput(error);
+
+        assert_eq!(
+            format!("{}", error_output),
+            error_output_with_suggestion(
+                "You are un-initialised.",
+                "Run \"wukong init\" to initialise Wukong's configuration."
+            )
+        );
+    }
+
+    #[test]
+    fn test_cli_error_output_format_with_caused_by() {
+        let io_error = std::io::Error::new(std::io::ErrorKind::NotFound, "File not exist");
+        let error = CliError::ConfigError(ConfigError::NotFound {
+            path: "not/path/to/file",
+            source: io_error,
+        });
+
+        let error_output = ErrorOutput(error);
+
+        assert_eq!(
+            format!("{}", error_output),
+            error_output_with_caused_by_and_suggestion(
+                "Config file not found at \"not/path/to/file\".",
+                "File not exist",
+                "Run \"wukong init\" to initialise configuration."
+            )
+        );
+    }
+}
