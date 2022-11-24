df18f925442d7545a7a474b3812e956ee96504b7
jk-gan <junkai@hey.com>
2022-11-24 14:48:20 +0800

    feat: handle deploy_for_this_build_is_currently_running error

--- src/graphql/deployment.rs
+++ src/graphql/deployment.rs
@@ -107,8 +107,12 @@ impl ExecuteCdPipeline {
                     code: error.message,
                     message: format!("Application `{}` not found.", application),
                 }),
+                "deploy_for_this_build_is_currently_running" => Err(APIError::ResponseError {
+                    code: error.message, 
+                    message: "Cannot submit this deployment request, since there is another running deployment with the same arguments is running on Spinnaker.\nYou can wait a few minutes and submit the deployment again.".to_string()
+                }),
                 _ => Err(APIError::ResponseError {
-                    code: error.message.clone(),
+                        code: error.message.clone(),
                     message: format!("{}", error),
                 }),
             })
