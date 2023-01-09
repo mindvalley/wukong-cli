74da615c43b716da273290e9878bb4d5ee5a320c
jk-gan <junkai@hey.com>
2023-01-09 14:05:08 +0800

    feat: customise verbosity help text

--- src/clap_app.rs
+++ src/clap_app.rs
@@ -1,6 +1,6 @@
 use crate::commands::CommandGroup;
 use clap::Parser;
-use clap_verbosity_flag::Verbosity;
+use clap_verbosity_flag::{LogLevel, Verbosity};
 
 /// A Swiss-army Knife CLI For Mindvalley Developers
 #[derive(Debug, Parser)]
@@ -15,7 +15,32 @@ pub struct ClapApp {
     pub application: Option<String>,
 
     #[clap(flatten)]
-    pub verbose: Verbosity,
+    pub verbose: Verbosity<ErrorLevel>,
+}
+
+#[derive(Debug)]
+pub struct ErrorLevel;
+
+impl LogLevel for ErrorLevel {
+    fn default() -> Option<log::Level> {
+        Some(log::Level::Error)
+    }
+
+    fn verbose_help() -> Option<&'static str> {
+        Some("Use verbose output (-vv very verbose and more output per occurrence)")
+    }
+
+    fn verbose_long_help() -> Option<&'static str> {
+        Some("Use verbos output. More output per occurrence.\n\nBy default, it'll only report errors.\n`-v` show warnings\n`-vv` show info\n`-vvv` show debug\n`-vvvv` show trace.")
+    }
+
+    fn quiet_help() -> Option<&'static str> {
+        Some("Do not print log message")
+    }
+
+    fn quiet_long_help() -> Option<&'static str> {
+        None
+    }
 }
 
 #[cfg(test)]
