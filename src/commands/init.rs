326ec2d92e2b7ead0d31a6d5296fcf4147952934
jk-gan <junkai@hey.com>
2023-02-17 14:40:13 +0800

    feat: check token expiry when user choosing exsiting account during init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -5,7 +5,9 @@ use crate::{
     graphql::QueryClientBuilder,
     output::colored_println,
 };
+use chrono::{DateTime, Local};
 use dialoguer::{theme::ColorfulTheme, Select};
+use openidconnect::RefreshToken;
 
 pub async fn handle_init() -> Result<bool, CliError> {
     println!("Welcome! This command will take you through the configuration of Wukong.\n");
@@ -23,10 +25,8 @@ pub async fn handle_init() -> Result<bool, CliError> {
         },
     };
 
-    let current_config = config.clone();
-
     let mut login_selections = vec!["Log in with a new account"];
-    if let Some(ref auth_config) = current_config.auth {
+    if let Some(ref auth_config) = config.auth {
         login_selections.splice(..0, vec![auth_config.account.as_str()]);
     };
 
@@ -37,7 +37,7 @@ pub async fn handle_init() -> Result<bool, CliError> {
                 .interact()?;
 
     // "Log in with a new account" is selected
-    let mut config = if selection == login_selections.len() - 1 {
+    let mut new_config = if selection == login_selections.len() - 1 {
         let mut config = Config::default();
 
         let auth_info = Auth::new(&config.core.okta_client_id).login().await?;
@@ -56,19 +56,43 @@ pub async fn handle_init() -> Result<bool, CliError> {
         colored_println!("You are logged in as: {}.\n", auth_info.account);
         config
     } else {
+        // check access token expiry
+        let mut current_config = config.clone();
+        let auth_config = current_config.auth.as_ref().unwrap();
+
+        let local: DateTime<Local> = Local::now();
+        let expiry = DateTime::parse_from_rfc3339(&auth_config.expiry_time)
+            .unwrap()
+            .with_timezone(&Local);
+
+        if local >= expiry {
+            let new_tokens = Auth::new(&config.core.okta_client_id)
+                .refresh_tokens(&RefreshToken::new(auth_config.refresh_token.clone()))
+                .await?;
+
+            current_config.auth = Some(AuthConfig {
+                account: auth_config.account.clone(),
+                subject: auth_config.subject.clone(),
+                id_token: new_tokens.id_token.clone(),
+                access_token: new_tokens.access_token.clone(),
+                expiry_time: new_tokens.expiry_time,
+                refresh_token: new_tokens.refresh_token,
+            });
+        }
+
         colored_println!("You are logged in as: {}.\n", login_selections[selection]);
 
         current_config
     };
 
     // SAFETY: The auth must not be None here
-    let auth_config = config.auth.as_ref().unwrap();
+    let auth_config = new_config.auth.as_ref().unwrap();
 
     // Calling API ...
     let client = QueryClientBuilder::default()
         .with_access_token(auth_config.id_token.clone())
         .with_sub(Some(auth_config.subject.clone()))
-        .with_api_url(config.core.wukong_api_url.clone())
+        .with_api_url(new_config.core.wukong_api_url.clone())
         .build()?;
 
     let applications_data: Vec<String> = client
@@ -92,7 +116,7 @@ pub async fn handle_init() -> Result<bool, CliError> {
         &applications_data[application_selection]
     );
 
-    config.core.application = applications_data[application_selection].clone();
+    new_config.core.application = applications_data[application_selection].clone();
 
     colored_println!(
         r#"
@@ -106,12 +130,14 @@ Some things to try next:
 
 * Run `wukong --help` to see the wukong command groups you can interact with. And run `wukong COMMAND help` to get help on any wukong command.
                      "#,
-        config.auth.as_ref().unwrap().account,
-        config.core.application
+        new_config.auth.as_ref().unwrap().account,
+        new_config.core.application
     );
 
     if let Some(ref config_file) = *CONFIG_FILE {
-        config.save(config_file).expect("Config file save failed");
+        new_config
+            .save(config_file)
+            .expect("Config file save failed");
     }
 
     Ok(true)
