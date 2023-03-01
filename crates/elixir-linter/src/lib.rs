a98b03ec1cf94916e9b8f1ebbc2a78b7c6a98e42
jk-gan <junkai@hey.com>
2023-03-01 16:57:37 +0800

    perf: cache the parse tree

--- crates/elixir-linter/src/lib.rs
+++ crates/elixir-linter/src/lib.rs
@@ -2,14 +2,14 @@ pub mod rules;
 
 use glob::Pattern;
 use ignore::{overrides::OverrideBuilder, WalkBuilder};
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 use miette::{Diagnostic, GraphicalReportHandler, NamedSource, SourceSpan};
 use rules::{
     no_env_in_dev_config::NoEnvInDevConfig, no_env_in_main_config::NoEnvInMainConfig,
     use_import_config_with_file_exists_checking::UseImportConfigWithFileExistsChecking, Rule,
 };
-use tree_sitter::{Language, Parser};
+use tree_sitter::{Language, Parser, Tree};
 
 #[derive(thiserror::Error, Debug, Diagnostic)]
 #[error("{name}")]
@@ -42,13 +42,27 @@ impl RuleExecutor {
         self
     }
 
-    pub fn run(&self, parser: &mut Parser, src: String, file_path: &str) -> Vec<LintError> {
+    pub fn run(
+        &self,
+        parser: &mut Parser,
+        parse_tree_map: &mut HashMap<String, Tree>,
+        src: String,
+        file_path: &str,
+    ) -> Vec<LintError> {
         let lint_errors: Vec<LintError> = self
             .rules
             .iter()
             .filter(|rule| Pattern::new(rule.glob()).unwrap().matches(file_path))
             .map(|rule| {
-                let parse_tree = parser.parse(&src, None).unwrap();
+                // cache the parse tree
+                let parse_tree = match parse_tree_map.get(file_path) {
+                    Some(tree) => tree,
+                    None => {
+                        let parse_tree = parser.parse(&src, None).unwrap();
+                        parse_tree_map.insert(file_path.to_string(), parse_tree);
+                        parse_tree_map.get(file_path).unwrap()
+                    }
+                };
                 rule.run(&parse_tree, src.clone(), file_path)
             })
             .flatten()
@@ -93,13 +107,16 @@ pub fn run() {
         elixir_lang,
     )));
 
-    let mut lint_time_takens = vec![];
     let mut count = 0;
 
     let program_load_time_taken = program_start.elapsed();
 
     let mut overrides = OverrideBuilder::new("./");
-    overrides.add("*.{ex,exs}").unwrap();
+    overrides.add("**/lib/**/*.{ex,exs}").unwrap();
+    overrides.add("**/test/**/*.{ex,exs}").unwrap();
+    overrides.add("**/config/**/*.{ex,exs}").unwrap();
+
+    let mut parse_tree_map = HashMap::new();
 
     for entry in WalkBuilder::new("./")
         .overrides(overrides.build().unwrap())
@@ -107,13 +124,16 @@ pub fn run() {
         .filter_map(|e| e.ok())
         .filter(|e| e.path().is_file())
     {
-        let now = Instant::now();
         count += 1;
-        println!("{}", entry.path().display());
 
         let file_path = entry.path().to_str().unwrap();
         let src = std::fs::read_to_string(file_path).unwrap();
-        let lint_errors = rule_executor.run(&mut parser, src.to_string(), &file_path);
+        let lint_errors = rule_executor.run(
+            &mut parser,
+            &mut parse_tree_map,
+            src.to_string(),
+            &file_path,
+        );
 
         lint_errors.iter().for_each(|lint_error| {
             let mut s = String::new();
@@ -123,15 +143,10 @@ pub fn run() {
 
             println!("{s}");
         });
-        lint_time_takens.push(now.elapsed());
     }
 
     let total_time_taken = program_start.elapsed();
-
-    let lint_time_taken = lint_time_takens
-        .into_iter()
-        .reduce(|a, b| a + b)
-        .unwrap_or_default();
+    let lint_time_taken = total_time_taken - program_load_time_taken;
 
     println!(
         "Total time taken: {:?} ({:?} to load, {:?} running {} checks)",
