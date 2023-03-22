df7f03df791d3c712e2afc9d09aa68f45cf003db
Fauzaan <mfauzaan@icloud.com>
2023-03-22 13:11:46 +0000

    test(vault): added test cases for login, update secret & get secrets client function

---
+++ src/commands/vault/update.rs
@@ -0,0 +1,18 @@
+use crate::{
+    commands::Context,
+    services::vault::Vault,
+    telemetry::{self, TelemetryData, TelemetryEvent},
+    CliError,
+};
+use wukong_telemetry_macro::wukong_telemetry;
+
+#[wukong_telemetry(command_event = "vault_list")]
+pub async fn update_secret(context: Context) -> Result<bool, CliError> {
+    // Call the vault client:
+    let _client = Vault::new()
+        .update_secret("wukong-cli/development", "test", "test2")
+        .await?;
+
+    print!("{:?}", _client);
+    Ok(true)
+}
--- src/services/vault/client.rs
+++ src/services/vault/client.rs
@@ -41,17 +41,26 @@ pub struct UpdateSecret {
 
 pub struct VaultClient {
     client: reqwest::Client,
+    base_url: String,
 }
 
 impl VaultClient {
-    const LOGIN: &str = "https://bunker.mindvalley.dev:8200/v1/auth/okta/login";
-    const VERIFY_TOKEN: &str = "https://bunker.mindvalley.dev:8200/v1/auth/token/lookup-self";
-    const FETCH_SECRETS: &str = "https://bunker.mindvalley.dev:8200/v1/secret/data";
-    const UPDATE_SECRET: &str = "https://bunker.mindvalley.dev:8200/v1/secret/data";
+    const LOGIN: &str = "/v1/auth/okta/login";
+    const VERIFY_TOKEN: &str = "/v1/auth/token/lookup-self";
+    const FETCH_SECRETS: &str = "/v1/secret/data";
+    const UPDATE_SECRET: &str = "/v1/secret/data";
 
     pub fn new() -> Self {
         let client = reqwest::Client::new();
-        Self { client }
+        Self {
+            client,
+            base_url: "https://bunker.mindvalley.dev:8200".to_string(),
+        }
+    }
+
+    pub fn with_base_url(mut self, base_url: String) -> Self {
+        self.base_url = base_url;
+        self
     }
 
     pub async fn login(
@@ -59,7 +68,12 @@ impl VaultClient {
         email: &str,
         password: &str,
     ) -> Result<reqwest::Response, reqwest::Error> {
-        let url = format!("{base_url}/{email}", base_url = Self::LOGIN, email = email);
+        let url = format!(
+            "{base_url}{base_path}/{email}",
+            base_path = Self::LOGIN,
+            email = email,
+            base_url = self.base_url
+        );
 
         let response = self
             .client
@@ -77,8 +91,9 @@ impl VaultClient {
         path: &str,
     ) -> Result<reqwest::Response, reqwest::Error> {
         let url = format!(
-            "{base_url}/{path}",
-            base_url = Self::FETCH_SECRETS,
+            "{base_url}{base_path}/{path}",
+            base_url = self.base_url,
+            base_path = Self::FETCH_SECRETS,
             path = path
         );
 
@@ -93,9 +108,15 @@ impl VaultClient {
     }
 
     pub async fn verify_token(&self, api_token: &str) -> Result<reqwest::Response, reqwest::Error> {
+        let url = format!(
+            "{base_url}{base_path}",
+            base_url = self.base_url,
+            base_path = Self::VERIFY_TOKEN,
+        );
+
         let response = self
             .client
-            .get(Self::VERIFY_TOKEN)
+            .get(url)
             .header("X-Vault-Token", api_token)
             .send()
             .await?;
@@ -111,8 +132,9 @@ impl VaultClient {
         value: &str,
     ) -> Result<reqwest::Response, reqwest::Error> {
         let url = format!(
-            "{base_url}/{path}",
-            base_url = Self::UPDATE_SECRET,
+            "{base_url}{base_path}/{path}",
+            base_url = self.base_url,
+            base_path = Self::UPDATE_SECRET,
             path = path
         );
 
@@ -130,5 +152,123 @@ impl VaultClient {
             .await?;
 
         Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use httpmock::prelude::*;
+
+    #[tokio::test]
+    async fn test_login() {
+        let server = MockServer::start();
+
+        let email = "test@example.com";
+        let password = "test_password";
+
+        let api_resp = r#"
+            {
+              "auth": {
+                "client_token": "test_token"
+                }
+            }"#;
+
+        let mock_server = server.mock(|when, then| {
+            when.method(POST).path_contains("/auth/okta/login");
+            then.status(200)
+                .header("content-type", "application/json; charset=UTF-8")
+                .body(api_resp);
+        });
+
+        let vault_client = VaultClient::new().with_base_url(server.base_url());
+        let response = vault_client.login(email, password).await;
+
+        mock_server.assert();
+        assert!(response.is_ok());
+
+        let response = response.unwrap();
+        assert_eq!(response.status(), 200);
+
+        let login_data = response.json::<Login>().await.unwrap();
+        assert_eq!(login_data.auth.client_token, "test_token");
+    }
+
+    #[tokio::test]
+    async fn fetch_secrets() {
+        let server = MockServer::start();
+
+        let api_token = "test_token";
+        let path = "devenv/test";
+
+        let api_resp = r#"
+            {
+              "data": {
+                "test2": "secret_token"
+                }
+            }"#;
+
+        let mock_server = server.mock(|when, then| {
+            when.method(GET)
+                .path_contains("/v1/secret/data")
+                .path_contains(path)
+                .header("X-Vault-Token", api_token);
+            then.status(200)
+                .header("content-type", "application/json; charset=UTF-8")
+                .body(api_resp);
+        });
+
+        let vault_client = VaultClient::new().with_base_url(server.base_url());
+        let response = vault_client.fetch_secrets(api_token, path).await;
+
+        mock_server.assert();
+        assert!(response.is_ok());
+
+        let response = response.unwrap();
+        assert_eq!(response.status(), 200);
+
+        let secrets = response.json::<FetchSecrets>().await.unwrap();
+
+        let data = secrets.data.data.get("test2").unwrap();
+        assert_eq!(data, "secret_token");
+    }
+
+    #[tokio::test]
+    async fn update_secret() {
+        let server = MockServer::start();
+
+        let api_token = "test_token";
+        let path = "devenv/test";
+        let key = "update_key";
+        let value = "updated_value";
+
+        let api_resp = r#"
+            {
+              "data": {
+                "test2": "secret_token"
+                }
+            }"#;
+
+        let mock_server = server.mock(|when, then| {
+            when.method(POST)
+                .path_contains("/v1/secret/data")
+                .body(format!(r#"{{"data":{{"{}":"{}"}}}}"#, key, value))
+                .header("X-Vault-Token", api_token);
+            then.status(200)
+                .header("content-type", "application/json; charset=UTF-8")
+                .body(api_resp);
+        });
+
+        let vault_client = VaultClient::new().with_base_url(server.base_url());
+
+        let response = vault_client
+            .update_secret(api_token, path, key, value)
+            .await;
+
+        mock_server.assert();
+        assert!(response.is_ok());
+
+        let response = response.unwrap();
+        assert_eq!(response.status(), 200);
     }
 }
