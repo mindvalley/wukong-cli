16c2c9f640a73b525ef9c9e32312f007af1b491f
jk-gan <junkai@hey.com>
2023-04-12 18:41:08 +0800

    feat: add command flags

--- src/commands/application/mod.rs
+++ src/commands/application/mod.rs
@@ -1,5 +1,5 @@
 use crate::error::CliError;
-use clap::{command, Args, Subcommand};
+use clap::{command, Args, Subcommand, ValueEnum};
 use info::handle_info;
 
 use self::logs_demo::handle_logs_demo;
@@ -21,6 +21,60 @@ pub enum ApplicationSubcommand {
     Info,
     /// Demo gcloud log
     LogsDemo,
+    /// Getting the logs of the applications from the Google Cloud Logging.
+    Logs {
+        /// (optional) The namespace to deploy to.
+        #[arg(long, value_enum, default_value_t=ApplicationNamespace::Prod)]
+        namespace: ApplicationNamespace,
+        /// (optional) The version that the deployment will perform against.
+        #[arg(long, value_enum, default_value_t=ApplicationVersion::Green)]
+        version: ApplicationVersion,
+        /// Only print out logs line with severity >= ERROR.
+        #[arg(long)]
+        errors: bool,
+        /// Show logs lines newer from relative duration, e.g 5m, 1h, 1d.
+        /// Also accept datetime in RFC 3339 format.
+        #[arg(long, short)]
+        since: Option<String>,
+        /// Show logs lines older than relative duration, e.g 30m, 2h, 2d.
+        /// Also accept datetime in RFC 3339 format.
+        #[arg(long, short)]
+        until: Option<String>,
+        /// Limiting the number of log entries to return.  
+        /// By default it will return the last 500 lines.
+        #[arg(long)]
+        limit: Option<String>,
+    },
+}
+
+#[derive(Debug, ValueEnum, Clone)]
+pub enum ApplicationVersion {
+    Blue,
+    Green,
+}
+
+impl ToString for ApplicationVersion {
+    fn to_string(&self) -> String {
+        match self {
+            ApplicationVersion::Blue => "Blue".to_string(),
+            ApplicationVersion::Green => "Green".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, ValueEnum, Clone)]
+pub enum ApplicationNamespace {
+    Prod,
+    Staging,
+}
+
+impl ToString for ApplicationNamespace {
+    fn to_string(&self) -> String {
+        match self {
+            ApplicationNamespace::Prod => "Prod".to_string(),
+            ApplicationNamespace::Staging => "Staging".to_string(),
+        }
+    }
 }
 
 impl Application {
@@ -30,6 +84,7 @@ impl Application {
         match &self.subcommand {
             ApplicationSubcommand::Info => handle_info(context).await,
             ApplicationSubcommand::LogsDemo => handle_logs_demo(context).await,
+            ApplicationSubcommand::Logs { .. } => Ok(true),
         }
     }
 }
