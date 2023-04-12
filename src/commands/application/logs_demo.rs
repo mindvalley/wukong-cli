a0062593eabf5244c2b2ec29f16c450d8322d6a4
jk-gan <junkai@hey.com>
2023-04-12 16:46:51 +0800

    feat: add list log entries function in GCloudClient

--- src/commands/application/logs_demo.rs
+++ src/commands/application/logs_demo.rs
@@ -6,29 +6,23 @@ use crate::{
 };
 
 pub async fn handle_logs_demo(_context: Context) -> Result<bool, CliError> {
-    let gcloud_client = GCloudClient::new().await?;
+    let auth_progress_bar = new_spinner_progress_bar();
+    auth_progress_bar.set_message("Checking authentication status...");
+
+    let client = GCloudClient::new().await;
+
+    auth_progress_bar.finish_and_clear();
 
     let progress_bar = new_spinner_progress_bar();
     progress_bar.set_message("Fetching log entries ... ");
 
-    let log = gcloud_client
+    let log = client
         .get_log_entries(LogEntriesOptions {
-            project_ids: None,
-            filter: None,
-            page_size: Some(5),
-            page_token: None,
-            order_by: None,
             resource_names: Some(vec!["projects/mv-stg-applications-hub".to_string()]),
+            page_size: Some(5),
+            ..Default::default()
         })
         .await?;
-
-    // let _log_tail = gcloud_client
-    //     .get_log_entries_tail(LogEntriesTailOptions {
-    //         resource_names: Some(vec!["projects/mv-stg-applications-hub".to_string()]),
-    //         filter: None,
-    //         buffer_window: None,
-    //     })
-    //     .await?;
 
     progress_bar.finish_and_clear();
 
