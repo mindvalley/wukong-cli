13ee2b84f4dae5143a536299045c77430d8bbc78
jk-gan <junkai@hey.com>
2023-04-17 19:50:39 +0800

    fix: fix github actions

--- build.rs
+++ build.rs
@@ -1,16 +1,20 @@
-use std::env;
+use std::env::{self, current_dir};
 
 fn main() {
-    let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let current_dir = current_dir().unwrap();
+    // let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     tonic_build::configure()
         .build_server(false)
         .out_dir("src/services/gcloud/api") // you can change the generated code's location
         .compile(
             &[format!(
                 "{}/proto/googleapis/google/logging/v2/logging.proto",
-                cargo_manifest_dir
+                current_dir.to_string_lossy()
             )],
-            &[format!("{}/proto/googleapis", cargo_manifest_dir)], // specify the root location to search proto dependencies
+            &[format!(
+                "{}/proto/googleapis",
+                current_dir.to_string_lossy()
+            )], // specify the root location to search proto dependencies
         )
         .unwrap();
 }
