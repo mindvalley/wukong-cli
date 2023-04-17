538d202947acfeb4c167c9ebcdcc215ecf195a2c
Fauzaan <mfauzaan@icloud.com>
2023-04-17 13:45:43 +0500

    PXP-517: [CLI] Rename "dev config-lint" command to "dev config lint" (#90)

--- src/commands/dev/mod.rs
+++ src/commands/dev/mod.rs
@@ -1,10 +1,7 @@
 mod config;
-mod config_lint;
 
 use crate::error::CliError;
 use clap::{Args, Subcommand};
-use config_lint::handle_config_lint;
-use std::path::PathBuf;
 
 #[derive(Debug, Args)]
 pub struct Dev {
@@ -14,12 +11,6 @@ pub struct Dev {
 
 #[derive(Debug, Subcommand)]
 pub enum DevSubcommand {
-    /// Linting the config and show possible warnings, as well as suggestion how to fix the config file.
-    ConfigLint {
-        /// The path to the project
-        #[arg(default_value = ".")]
-        path: PathBuf,
-    },
     /// This command group contains the commands to interact with the config secrets with bunker.
     Config(config::Config),
 }
@@ -27,7 +18,6 @@ pub enum DevSubcommand {
 impl Dev {
     pub async fn handle_command(&self) -> Result<bool, CliError> {
         match &self.subcommand {
-            DevSubcommand::ConfigLint { path } => handle_config_lint(path),
             DevSubcommand::Config(config) => config.handle_command().await,
         }
     }
