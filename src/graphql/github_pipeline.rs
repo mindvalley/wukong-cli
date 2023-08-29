9cead603365beda6565f91263a017d29efa5496c
Mohamed Fauzaan <mfauzaan@icloud.com>
2023-08-29 08:16:32 +0000

    feat: update application_not_found with correct error code

--- src/graphql/github_pipeline.rs
+++ src/graphql/github_pipeline.rs
@@ -64,9 +64,8 @@ impl GithubCdPipelineQuery {
 
         let response = client
             .call_api::<Self>(variables, |_, error| match error.message.as_str() {
-                "application_not_found" => Err(APIError::ResponseError {
-                    code: error.message,
-                    message: format!("Application `{application}` not found."),
+                "application_not_found" => Err(APIError::ResponseError { code: "ci_status_application_not_found".to_string(), message: 
+    "Could not find the application associated with this Git repo.\n\tEither you're not in the correct working folder for your application, or there's a misconfiguration.".to_string()
                 }),
                 _ => Err(APIError::ResponseError {
                     code: error.message.clone(),
