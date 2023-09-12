c1a2859e49f043adabf14f45e499ac55560fcfe4
Mohamed Fauzaan <mfauzaan@icloud.com>
2023-09-12 07:57:32 +0000

    style: fix clappy reported issues

--- cli/src/commands/tui/mod.rs
+++ cli/src/commands/tui/mod.rs
@@ -134,7 +134,7 @@ pub async fn start_ui(app: &Arc<Mutex<App>>) -> std::io::Result<bool> {
         terminal.draw(|frame| ui::draw(frame, &mut app_ref))?;
 
         // move cursor to the top left corner to avoid screen scrolling:
-        terminal.set_cursor(1 + 0, 1)?;
+        terminal.set_cursor(1, 1)?;
 
         let result = match event_manager.next().unwrap() {
             events::Event::Input(key) => app_ref.handle_input(key).await,
