68c0631e6f7955c52a7e654a5e3df281d61e492d
jk-gan <junkai@hey.com>
2023-10-02 11:13:06 +0900

    fix: fix scroll bugs

--- cli/src/commands/tui/ui/logs.rs
+++ cli/src/commands/tui/ui/logs.rs
@@ -268,9 +268,14 @@ fn render_log_entries<B: Backend>(frame: &mut Frame<'_, B>, logs_area: Rect, sta
                 log_entries.push(Line::from(line));
             }
 
+            state.logs_vertical_scroll = 0;
             state.logs_vertical_scroll_state = state
                 .logs_vertical_scroll_state
-                .content_length(filtered_log_entries.len() as u16);
+                .position(state.logs_vertical_scroll as u16);
+
+            state.logs_vertical_scroll_state = state
+                .logs_vertical_scroll_state
+                .content_length(log_entries.len() as u16);
 
             log_entries
         }
@@ -359,9 +364,13 @@ fn render_log_entries<B: Backend>(frame: &mut Frame<'_, B>, logs_area: Rect, sta
                 log_entries.push(Line::from(line));
             }
 
+            state.logs_vertical_scroll = 0;
             state.logs_vertical_scroll_state = state
                 .logs_vertical_scroll_state
-                .content_length(filtered_log_entries.len() as u16);
+                .position(state.logs_vertical_scroll as u16);
+            state.logs_vertical_scroll_state = state
+                .logs_vertical_scroll_state
+                .content_length(log_entries.len() as u16);
 
             log_entries
         }
@@ -378,90 +387,17 @@ fn render_log_entries<B: Backend>(frame: &mut Frame<'_, B>, logs_area: Rect, sta
             })
             .collect()
     };
-    // let log_entries = if state.search_bar_input.input.is_empty() {
-    //     state.logs_vertical_scroll_state = state
-    //         .logs_vertical_scroll_state
-    //         .content_length(state.log_entries_length as u16);
-    //
-    //     state
-    //         .log_entries
-    //         .iter()
-    //         .map(|log_entry| {
-    //             Line::styled(format!("{}", log_entry), Style::default().fg(Color::White))
-    //         })
-    //         .collect()
-    // } else {
-    //     let regex = Regex::new(&format!(r"(?i){}", state.search_bar_input.input.trim())).unwrap();
-    //
-    //     let filtered_log_entries = state
-    //         .log_entries
-    //         .iter()
-    //         .filter(|each| regex.is_match(&each.to_string()))
-    //         .collect::<Vec<_>>();
-    //
-    //     let mut log_entries = Vec::new();
-    //     for each in filtered_log_entries.iter() {
-    //         let output_string = each.to_string();
-    //
-    //         let mut matches: Vec<(usize, usize)> = Vec::new();
-    //         for found in regex.find_iter(&output_string.clone()) {
-    //             let start = found.start();
-    //             let end = found.end();
-    //             matches.push((start, end));
-    //         }
-    //
-    //         let mut line = Vec::new();
-    //         let mut last_pos = 0;
-    //         for (index, m) in matches.iter().enumerate() {
-    //             if index == 0 {
-    //                 line.push(Span::styled(
-    //                     output_string[0..m.0].to_string(),
-    //                     Style::default().fg(Color::White),
-    //                 ));
-    //             }
-    //
-    //             if last_pos != 0 {
-    //                 line.push(Span::styled(
-    //                     output_string[last_pos..m.0].to_string(),
-    //                     Style::default().fg(Color::White),
-    //                 ));
-    //             }
-    //
-    //             line.push(Span::styled(
-    //                 output_string[m.0..m.1].to_string(),
-    //                 Style::default().fg(Color::Cyan),
-    //             ));
-    //
-    //             if index == matches.len() - 1 {
-    //                 line.push(Span::styled(
-    //                     output_string[m.1..].to_string(),
-    //                     Style::default().fg(Color::White),
-    //                 ));
-    //             }
-    //
-    //             last_pos = m.1;
-    //         }
-    //
-    //         log_entries.push(Line::from(line));
-    //     }
-    //
-    //     state.logs_vertical_scroll_state = state
-    //         .logs_vertical_scroll_state
-    //         .content_length(filtered_log_entries.len() as u16);
-    //
-    //     log_entries
-    // };
 
-    let paragraph =
-        Paragraph::new(log_entries).block(Block::default().padding(Padding::new(1, 1, 0, 0)));
-    // we can't use wrap if we want to scroll to bottom
-    // because we don't know the state of the render
-    // waiting this https://github.com/ratatui-org/ratatui/issues/136
-    // .wrap(Wrap { trim: true })
-    // .scroll((
-    //     state.logs_vertical_scroll as u16,
-    //     state.logs_horizontal_scroll as u16,
-    // ));
+    let paragraph = Paragraph::new(log_entries)
+        .block(Block::default().padding(Padding::new(1, 1, 0, 0)))
+        // we can't use wrap if we want to scroll to bottom
+        // because we don't know the state of the render
+        // waiting this https://github.com/ratatui-org/ratatui/issues/136
+        // .wrap(Wrap { trim: true })
+        .scroll((
+            state.logs_vertical_scroll as u16,
+            state.logs_horizontal_scroll as u16,
+        ));
 
     frame.render_widget(paragraph, logs_area);
 }
