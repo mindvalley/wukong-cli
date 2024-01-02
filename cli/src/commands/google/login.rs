4fc7b139423628e8832f2745eab4b845a32f2ebb
Mohamed Fauzaan <mfauzaan@icloud.com>
2024-01-02 07:16:29 +0000

    chore: remove unused println

--- cli/src/commands/google/login.rs
+++ cli/src/commands/google/login.rs
@@ -4,8 +4,7 @@ pub async fn handle_login() -> Result<bool, WKCliError> {
     let loader = new_spinner();
     loader.set_message("Logging in to Google Cloud ...");
 
-    let test = auth::google_cloud::get_token_or_login().await;
-    println!("{:?}", test);
+    auth::google_cloud::get_token_or_login().await;
 
     loader.finish_with_message(
         "Successfully logged in to Google Cloud. You can now use Wukong to manage your Google Cloud resources.\n",
